7508b3afece9c1d0287ec3f82d5bd20e1052c7c9
Aleksa Pavlovic <pavlovicaleksa@yahoo.com>
2023-03-13 22:59:45 +0100

    remove bincode

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,20 @@
 fn main() {
-    let a: f32 = bincode::deserialize(&[100, 192, 65, 63]).unwrap();
-    println!("{:?}", a);
+    let a = f32::from_bits(1061273700);
+    println!("{}", a);
+    println!();
 
     let sin = a.sin();
-    println!("sin {:?}", bincode::serialize(&sin).unwrap());
+    println!("sin\t\t{:x}", sin.to_bits());
     let cos = a.cos();
-    println!("cos {:?}", bincode::serialize(&cos).unwrap());
+    println!("cos\t\t{:x}", cos.to_bits());
 
     let sincos_tuple1 = (sin, cos);
-    println!(
-        "sincos_tuple1 {:?}",
-        bincode::serialize(&sincos_tuple1).unwrap()
-    );
-    println!(
-        "sincos_tuple1.0 {:?}",
-        bincode::serialize(&sincos_tuple1.0).unwrap()
-    );
-    println!(
-        "sincos_tuple1.1 {:?}",
-        bincode::serialize(&sincos_tuple1.1).unwrap()
-    );
-
+    println!("tuple1.sin\t{:x}", sincos_tuple1.0.to_bits());
+    println!("tuple1.cos\t{:x}", sincos_tuple1.1.to_bits());
     let sincos_tuple2 = (a.sin(), a.cos());
-    println!(
-        "sincos_tuple2 {:?}",
-        bincode::serialize(&sincos_tuple2).unwrap()
-    );
-    println!(
-        "sincos_tuple2.0 {:?}",
-        bincode::serialize(&sincos_tuple2.0).unwrap()
-    );
-    println!(
-        "sincos_tuple2.1 {:?}",
-        bincode::serialize(&sincos_tuple2.1).unwrap()
-    );
-
+    println!("tuple2.sin\t{:x}", sincos_tuple2.0.to_bits());
+    println!("tuple2.cos\t{:x}", sincos_tuple2.1.to_bits());
     let sincos = a.sin_cos();
-    println!("sincos {:?}", bincode::serialize(&sincos).unwrap());
-    println!("sincos.0 {:?}", bincode::serialize(&sincos.0).unwrap());
-    println!("sincos.1 {:?}", bincode::serialize(&sincos.1).unwrap());
+    println!("sincos.sin\t{:x}", sincos.0.to_bits());
+    println!("sincos.cos\t{:x}", sincos.1.to_bits());
 }

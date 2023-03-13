d400c67537a92e4ffabadd078350b6d94bd68890
Aleksa Pavlovic <pavlovicaleksa@yahoo.com>
2023-03-14 00:53:25 +0100

    shorten the code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 fn main() {
     let a: f32 = 0.7568419;
 
-    let sin1 = a.sin();
-    println!("{:x}", sin1.to_bits());
-    let sin2 = a.sin();
-    println!("{:x}", sin2.to_bits());
-
-    println!("{}", a);
+    println!("{:x}", a.sin().to_bits());
+    println!("{:x}", a.sin().to_bits());
+    println!("{a}");
 }

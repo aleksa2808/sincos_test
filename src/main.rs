9be470795b088ff03d8902eab29850484a7da4d3
Aleksa Pavlovic <pavlovicaleksa@yahoo.com>
2023-03-13 23:43:03 +0100

    reduce the example

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,10 @@
 fn main() {
-    let a = f32::from_bits(1061273700);
+    let a: f32 = 0.7568419;
+
+    let sin1 = a.sin();
+    println!("{:x}", sin1.to_bits());
+    let sin2 = a.sin();
+    println!("{:x}", sin2.to_bits());
+
     println!("{}", a);
-    println!();
-
-    let sin = a.sin();
-    println!("sin\t\t{:x}", sin.to_bits());
-    let cos = a.cos();
-    println!("cos\t\t{:x}", cos.to_bits());
-
-    let sincos_tuple1 = (sin, cos);
-    println!("tuple1.sin\t{:x}", sincos_tuple1.0.to_bits());
-    println!("tuple1.cos\t{:x}", sincos_tuple1.1.to_bits());
-    let sincos_tuple2 = (a.sin(), a.cos());
-    println!("tuple2.sin\t{:x}", sincos_tuple2.0.to_bits());
-    println!("tuple2.cos\t{:x}", sincos_tuple2.1.to_bits());
-    let sincos = a.sin_cos();
-    println!("sincos.sin\t{:x}", sincos.0.to_bits());
-    println!("sincos.cos\t{:x}", sincos.1.to_bits());
 }

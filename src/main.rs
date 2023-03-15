5c2b684904ae3b640a8a973f24f2654926c66ac0
Aleksa Pavlovic <pavlovicaleksa@yahoo.com>
2023-03-15 16:55:07 +0100

    simplify

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
     let a: f32 = 0.7568419;
-    println!("{:?}", a);
+    format!("{}", a);
 
     let tuple = (a.sin(), a.cos());
-    println!("tuple.1 {:x}", tuple.0.to_bits());
-    format!("tuple.1 {:x}", tuple.1.to_bits());
+    println!("tuple.sin {:x}", tuple.0.to_bits());
+    format!("{}", tuple.1);
 
     let sincos = a.sin_cos();
-    println!("sincos.0 {:x}", sincos.0.to_bits());
-    format!("sincos.1 {:x}", sincos.1.to_bits());
+    println!("sincos.sin {:x}", sincos.0.to_bits());
+    format!("{}", sincos.1);
 }

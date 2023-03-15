52bead3997f4c38f6674346bae86a2f504a29895
Aleksa Pavlovic <pavlovicaleksa@yahoo.com>
2023-03-15 16:53:41 +0100

    remove cos printing while still using the var

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ fn main() {
 
     let tuple = (a.sin(), a.cos());
     println!("tuple.1 {:x}", tuple.0.to_bits());
-    println!("tuple.1 {:x}", tuple.1.to_bits());
+    format!("tuple.1 {:x}", tuple.1.to_bits());
 
     let sincos = a.sin_cos();
     println!("sincos.0 {:x}", sincos.0.to_bits());
-    println!("sincos.1 {:x}", sincos.1.to_bits());
+    format!("sincos.1 {:x}", sincos.1.to_bits());
 }

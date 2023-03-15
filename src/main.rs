50f730b8c9989871d32708136a64b145b9b54503
Aleksa Pavlovic <pavlovicaleksa@yahoo.com>
2023-03-15 16:49:41 +0100

    simplify

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
 fn main() {
     let a: f32 = 0.7568419;
-    println!("{:?}", a);
 
-    let sin = a.sin();
-    println!("sin {:x}", sin.to_bits());
-    let cos = a.cos();
-    println!("cos {:x}", cos.to_bits());
-
-    let sincos_tuple1 = (sin, cos);
-    println!("sincos_tuple1.0 {:x}", sincos_tuple1.0.to_bits());
-    println!("sincos_tuple1.1 {:x}", sincos_tuple1.1.to_bits());
-
-    let sincos_tuple2 = (a.sin(), a.cos());
-    println!("sincos_tuple2.1 {:x}", sincos_tuple2.0.to_bits());
-    println!("sincos_tuple2.1 {:x}", sincos_tuple2.1.to_bits());
+    let tuple = (a.sin(), a.cos());
+    println!("tuple.1 {:x}", tuple.0.to_bits());
+    println!("tuple.1 {:x}", tuple.1.to_bits());
 
     let sincos = a.sin_cos();
     println!("sincos.0 {:x}", sincos.0.to_bits());
